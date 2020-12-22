3af755e0fe48d90871c36c897b29dc14d0185c51
Gabriel Rufino <contato@gabrielrufino.com>
2020-12-22 10:51:35 -0300

    Setup to database

---
+++ db.js
@@ -0,0 +1,13 @@
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+db
+  .defaults({
+    speeds: [],
+  })
+  .write()
+
+module.exports = db
