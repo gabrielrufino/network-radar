6e122999193ecc2dd071874cb901e9b22e65df2d
Gabriel Rufino <contato@gabrielrufino.com>
2020-12-22 11:32:04 -0300

    Collecting speeds every hour

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-const speedTest = require('speedtest-net');
- 
-(async () => {
-  try {
-    const result = await speedTest({
-      acceptGdpr: true,
-      acceptLicense: true
-    });
+const speedTest = require('speedtest-net')
+const CronJob = require('cron').CronJob
 
-    console.log(result)
-  } catch (err) {
-    console.log(err.message);
-  } finally {
-    process.exit(0);
-  }
-})();
+const db = require('./db')
+const { bytesToBits, toMega } = require('./conversor')
+
+const job = new CronJob('0 0 * * * *', function() {
+  const startedAt = Date()
+
+  speedTest({
+    acceptGdpr: true,
+    acceptLicense: true
+  })
+    .then(result => {
+      const finishedAt = Date()
+
+      const speed = {
+        download: toMega(bytesToBits(result.download.bandwidth)),
+        upload: toMega(bytesToBits(result.upload.bandwidth)),
+        startedAt,
+        finishedAt
+      }
+  
+      db.get('speeds').push(speed).write()
+    })
+    .catch(error => console.error(error.message))
+}, null, true, 'America/Recife')
+
+job.start()
