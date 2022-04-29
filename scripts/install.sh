ce17403022e7f29f9c14d444b6e20ecff1988ec5
Gabriel Rufino <contato@gabrielrufino.com>
2022-04-29 18:53:12 -0300

    Update install

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,9 @@
-cd ~
+cd /home/pi
 git clone https://github.com/gabrielrufino/network-radar.git
 cd network-radar
 
 npm ci
 
+cp network-radar.service /etc/systemd/system/
+systemctl start network-radar
+systemctl enable network-radar
