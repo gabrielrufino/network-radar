36ef51a2ecc101c0f961c985c4bb83c837f9ebfa
Gabriel Rufino <contato@gabrielrufino.com>
2022-04-29 08:08:39 -0300

    Using environment variables for database

--- src/database.js
+++ src/database.js
@@ -1,9 +1,10 @@
 'use strict'
 
 const { Sequelize } = require('sequelize')
-const { join } = require('path')
+
+const { DATABASE_TYPE, DATABASE_URL } = process.env
 
 module.exports = new Sequelize({
-  dialect: 'sqlite',
-  storage: join(process.cwd(), 'database.sqlite')
+  dialect: DATABASE_TYPE,
+  storage: DATABASE_URL
 })
